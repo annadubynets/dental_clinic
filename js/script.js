d7ac0f9d7d57bd0f23b5674e0d89e6c3e0ad5713
Anna Dubynets <arogulina@gmail.com>
2020-09-02 14:34:30 +0300

    add site

---
+++ js/script.js
@@ -0,0 +1,35 @@
+$('.services-section .owl-carousel').owlCarousel({
+    loop:true,
+    margin:30,
+    nav:true,
+    responsive:{
+        0:{
+            items:1,
+            stagePadding: 50,
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:3
+        }
+    }
+});
+
+$('.articles-section .owl-carousel').owlCarousel({
+    loop:true,
+    margin:30,
+    nav:true,
+    responsive:{
+        0:{
+            items:1,
+            stagePadding: 50,
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+});
